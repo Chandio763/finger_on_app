184abfe74bb51aef1694c496e885a4264039f8c6
Chandio763 <kashikha763@gmail.com>
2023-04-18 16:02:00 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         theme: ThemeData(
             primarySwatch: primaryGreen,
-            iconTheme: IconThemeData(
+            iconTheme: const IconThemeData(
               color: primaryGreen,
             )),
         home: const LoginPage(),
